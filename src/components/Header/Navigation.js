717733156b930a3ba80427576f9a355649c52ddd
Josh Allan <allan.josh07@gmail.com>
2021-01-14 10:49:19 -0500

    Updated the fonts sitewide. Also added the icon in the name.

--- src/components/Header/Navigation.js
+++ src/components/Header/Navigation.js
@@ -5,7 +5,9 @@ function Navigation() {
   return (
     <Navbar className="navigationbar" bg="primary" expand="lg">
       <div className="container">
-        <Navbar.Brand href="#home">Employ•able</Navbar.Brand>
+        <Navbar.Brand href="#home">
+          Employ <i class="fas fa-user-circle fa-xs"></i> able
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
@@ -14,21 +16,21 @@ function Navigation() {
               href="https://joshallan.dev"
               target="_blank"
             >
-              <i className="fas fa-at"> </i> Created By Josh Allan
+              <i className="fas fa-at"> &nbsp;</i> Created By Josh Allan
             </Nav.Link>
             <Nav.Link
               className="mx-2"
               href="https://github.com/jallan07/employable-react-directory"
               target="_blank"
             >
-              <i className="fab fa-github"> </i> View Source Code
+              <i className="fab fa-github"> &nbsp;</i> View Source Code
             </Nav.Link>
             <Nav.Link
               className="mx-2"
               href="https://reactjs.org/"
               target="_blank"
             >
-              <i className="fab fa-react"> </i>Powered By React
+              <i className="fab fa-react"> &nbsp;</i>Powered By React
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
