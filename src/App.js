7471f61f7b42c6406bb98ed9e379aa0d7f00c5a5
Josh Allan <allan.josh07@gmail.com>
2021-01-13 22:49:25 -0500

    build packages

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Table from './components/Table/Table';
 import Header from './components/Header/Header';
 
 // router
-import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 // styles
 import './App.css';
 
@@ -15,8 +15,6 @@ function App() {
         <Header />
         <Table />
       </Route>
-      {/* redirect any other page requests back to the home page for a true singlepage experience*/}
-      <Redirect from="*" to="/employable-react-directory/" />
     </Router>
   );
 }
